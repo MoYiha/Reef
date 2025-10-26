8b722cfd3118fdb11caedae63fbfdeef21a399d7
invoke <purwarpranav80@gmail.com>
2025-10-27 01:21:44 +0900

    Enable minification and resource shrinking in release
    Enable resource shrinking and code minification for release builds.

--- Reef/build.gradle.kts
+++ Reef/build.gradle.kts
@@ -27,8 +27,8 @@ android {
     buildTypes {
         release {
             // Causing issues smh
-            isMinifyEnabled = false
-            isShrinkResources = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
             )
